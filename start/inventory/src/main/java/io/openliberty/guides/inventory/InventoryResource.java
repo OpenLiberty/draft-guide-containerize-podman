e445de93052ebe1c0579a741a5b168717cf2cded
Gilbert Kwan <gkwan@ca.ibm.com>
2022-02-01 13:20:02 -0500

    Update InventoryResource.java

--- start/inventory/src/main/java/io/openliberty/guides/inventory/InventoryResource.java
+++ start/inventory/src/main/java/io/openliberty/guides/inventory/InventoryResource.java
@@ -1,6 +1,6 @@
 // tag::copyright[]
 /*******************************************************************************
- * Copyright (c) 2017, 2020 IBM Corporation and others.
+ * Copyright (c) 2017, 2022 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -43,7 +43,7 @@ public class InventoryResource {
     Properties props = systemClient.getProperties(hostname);
     if (props == null) {
       return Response.status(Response.Status.NOT_FOUND)
-                     .entity("{ \"error\" : \"Unknown hostname or the system service " 
+                     .entity("{ \"error\" : \"Unknown hostname or the system service "
                      + "may not be running on " + hostname + "\" }")
                      .build();
     }
