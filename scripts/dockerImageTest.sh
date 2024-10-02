78ff2dbf69fa8fc5e130fa20e273fd4688f4029f
Gilbert Kwan <gkwan@ca.ibm.com>
2024-10-02 11:13:14 -0400

    fix the dockerImageTest.sh (#114)
    * Bump org.junit.jupiter:junit-jupiter-engine in /finish
    
    Bumps [org.junit.jupiter:junit-jupiter-engine](https://github.com/junit-team/junit5) from 5.11.0 to 5.11.1.
    - [Release notes](https://github.com/junit-team/junit5/releases)
    - [Commits](https://github.com/junit-team/junit5/compare/r5.11.0...r5.11.1)
    
    ---
    updated-dependencies:
    - dependency-name: org.junit.jupiter:junit-jupiter-engine
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump org.junit.jupiter:junit-jupiter-engine in /start
    
    Bumps [org.junit.jupiter:junit-jupiter-engine](https://github.com/junit-team/junit5) from 5.11.0 to 5.11.1.
    - [Release notes](https://github.com/junit-team/junit5/releases)
    - [Commits](https://github.com/junit-team/junit5/compare/r5.11.0...r5.11.1)
    
    ---
    updated-dependencies:
    - dependency-name: org.junit.jupiter:junit-jupiter-engine
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Update dockerImageTest.sh
    
    * Update dockerImageTest.sh
    
    * Update dockerImageTest.sh
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- scripts/dockerImageTest.sh
+++ scripts/dockerImageTest.sh
@@ -14,7 +14,7 @@ sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install
 cat ../start/system/pom.xml ../start/inventory/pom.xml ../finish/system/pom.xml ../finish/inventory/pom.xml
 
 if [[ "$OL_LEVEL" != "" ]]; then
-  sed -i "s;FROM icr.io/appcafe/open-liberty:kernel-slim-java11-openj9-ubi;FROM cp.stg.icr.io/cp/olc/open-liberty-vnext:$OL_LEVEL-full-java11-openj9-ubi;g" system/Dockerfile inventory/Dockerfile
+  sed -i "s;FROM icr.io/appcafe/open-liberty:kernel-slim-java11-openj9-ubi;FROM cp.stg.icr.io/cp/olc/open-liberty-vnext:$OL_LEVEL-full-java11-openj9-ubi;g" system/Containerfile inventory/Containerfile
 else
   sed -i "s;FROM icr.io/appcafe/open-liberty:kernel-slim-java11-openj9-ubi;FROM cp.stg.icr.io/cp/olc/open-liberty-daily:full-java11-openj9-ubi;g" system/Containerfile inventory/Containerfile
 fi
@@ -22,7 +22,7 @@ sed -i "s;RUN features.sh;#RUN features.sh;g" system/Containerfile inventory/Con
 sed -i "s;RUN configure.sh;#RUN configure.sh;g" system/Containerfile inventory/Containerfile
 cat system/Containerfile inventory/Containerfile
 if [[ "$OL_LEVEL" != "" ]]; then
-  sed -i "s;FROM icr.io/appcafe/open-liberty:full-java11-openj9-ubi;FROM cp.stg.icr.io/cp/olc/open-liberty-vnext$OL_LEVEL-full-java11-openj9-ubi;g" system/Containerfile-full inventory/Containerfile-full
+  sed -i "s;FROM icr.io/appcafe/open-liberty:full-java11-openj9-ubi;FROM cp.stg.icr.io/cp/olc/open-liberty-vnext:$OL_LEVEL-full-java11-openj9-ubi;g" system/Containerfile-full inventory/Containerfile-full
 else
   sed -i "s;FROM icr.io/appcafe/open-liberty:full-java11-openj9-ubi;FROM cp.stg.icr.io/cp/olc/open-liberty-daily:full-java11-openj9-ubi;g" system/Containerfile-full inventory/Containerfile-full
 fi
